3f6dbd3e766c9286db48c6c7babf0b4384ba01a2
Willjon <163702289+williamjohan@users.noreply.github.com>
2024-05-08 09:13:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/peminjaman_sarpras/Login.java
@@ -0,0 +1,11 @@
+package com.example.peminjaman_sarpras;
+
+public class Login {
+    private String user;
+    private String password ;
+
+    public Login(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+}
